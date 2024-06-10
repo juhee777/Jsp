565a6f1bfd9ef506b4b16d7fb6bf1aebce4868a7
admin <admin@YD01-02>
2024-06-10 17:36:56 +0900

    BoardWeb추가

---
+++ BoardWeb/src/test/java/co/yedam/common/AppTest.java
@@ -0,0 +1,40 @@
+package co.yedam.common;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import co.yedam.mapper.StudentMapper;
+import co.yedam.vo.Student;
+
+public class AppTest {
+	public static void main(String[] args) {
+		SqlSessionFactory sqlSessionFactory = DataSource.getInstance();
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		
+		//interface - 구현객체
+		StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+		
+		Student std = new Student();
+		std.setStdNo("s0020");
+		std.setStdName("김영만");
+		std.setPhone("010-8566-0342");
+		std.setBldType("A");
+		
+//		sqlSession.insert("co.yedam.mapper.StudentMapper.insertStudent", std);
+//		sqlSession.commit();
+		
+		sqlSession.update("co.yedam.mapper.StudentMapper.updateStudent", std);
+		mapper.updateStudent(std);
+		sqlSession.commit();
+		
+		List<Student> list //
+				= sqlSession.selectList("co.yedam.mapper.StudentMapper.selectBlog");
+//		        = mapper.selectBlog();
+		for(Student std1 : list) {
+			System.out.println(std1.toString());
+		}
+		
+	}
+}
