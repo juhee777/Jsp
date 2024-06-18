447eb31f83eae621c7268ddc16632b99afd43edb
master <master@DESKTOP-4FDRL6R>
2024-06-19 02:21:26 +0900

    wntjr

--- BoardWeb/src/main/webapp/js/ary3.js
+++ BoardWeb/src/main/webapp/js/ary3.js
@@ -4,7 +4,7 @@
 let sum =
 	[10, 20, 30, 40, 50].reduce(function(acc, elem, idx, ary) {//acc: 누적값 
 		console.log(acc, elem, idx, ary);
-		if(elem > acc){
+		if(elem > acc){//elem이 acc보다 크면 acc를 반환, 그렇지 않으면 elem을 반환
 			return acc;
 		}
 		return elem;
--- BoardWeb/src/main/webapp/js/calendar.js
+++ BoardWeb/src/main/webapp/js/calendar.js
@@ -10,78 +10,89 @@ document.querySelectorAll('.container-fluid table.table')
 document.getElementById('fruit').remove();
 
 //월을 변경하면 달력을 출력하는 이벤트를 등록(selectMonth)
-function makeCal(value){
+function makeCal(value){//월을 변경할 때마다 실행
+	console.log(value);
 	makeCalendar(parseInt(value.target.value))
 }
-
-document.getElementById('#selectMonth').addEventListener('change', makeCal);
+//#selectMonth요소 값을 변경하면 makeCal 함수 실행
+document.querySelector('#selectMonth').addEventListener('change', makeCal);
 
 //달력을 첫째날 계산하는 함수, 마지막날 계산하는 함수
-function getFirstDay(month = 6){
+function getFirstDay(month = 6){ 
 	switch(month){
-		case 5: //5월달
+		case 5: //5월달의 첫째날은 수요일이니 4(일요일이 1)
 			return 4;
 		case 7: //7월달
 			return 2;
 	}
+	return 7;
 }
 
 function getLastDate(month = 6){
 	switch(month){
-		case 5:
+		case 5: //5월, 7월 마지막 날은 31일
 		case 7:
 			return 31;
 	}
-	return 30;
+	return 30; 
 }
+
+//달력생성
 makeCalendar(6);
 function makeCalendar(month = 6){
 	let firstDay = getFirstDay(month); //1일의 위치를 지정하기 위해서 공란의 갯수를 반환
 	let lastDate = getLastDate(month); //월의 마지막날을 반환하는 함수
+	
+	document.getElementById('show').innerHTML = ''; //이전에 출력된 달력 삭제
+	
 	// table 생성. border="2"
-	let tbl = document.createElement('table');
+	let tbl = document.createElement('table'); //table요소 생성
 	tbl.setAttribute('class', 'table');
 	let thd = document.createElement('thead');
 	let tbd = document.createElement('tbody');
 	
 	//thead 하위요소
-	let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'];
-	let tr = document.createElement('tr');
+	let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat']; //요일정보
+	let tr = document.createElement('tr');//<tr>생성
 	days.forEach(day =>{
-		let td = document.createElement('th');
-		td.innerHTML = day;
-		tr.appendChild(td);
+		let td = document.createElement('th');//<th>생성
+		td.innerHTML = day; //day(요일이름 배열)이 <th>에 하나씩 생성
+		tr.appendChild(td); //<tr><th></th></tr>
 	})
-	thd.appendChild(tr);
+	thd.appendChild(tr);//<thead><tr></tr></thead>
 	
 	//tbody 하위요소.
-	tr = document.createElement('tr');
+	tr = document.createElement('tr'); //<tbody><tr>
+	
 	//빈날짜
-	for(let i = 1; i < firstDay; i++){
+	for(let i = 1; i < firstDay; i++){//빈칸 6칸을 넣고 첫 시작 1이 7번째 자리
 		let td = document.createElement('td');
 		tr.appendChild(td);
 	}
+	
 	//날짜출력
-	for(let d = 1; d <= lastDate; d++){
+	for(let d = 1; d <= lastDate; d++){ //1부터 해당월의 마지막 날짜까지 출력
 		let td = document.createElement('td');
 		td.innerHTML = d;
-		tr.appendChild(td);
-		
+		tr.appendChild(td); 
 		if((d+firstDay-1) % 7 == 0){ //7일마다 줄바꿈.
-			tbd.appendChild(tr); 
-			tr = document.createElement('tr');
+			tbd.appendChild(tr); //<tbody><tr>
+			tr = document.createElement('tr'); //줄바꿈
 		}
 		//토요일 배경색은 파란색
 		if((d+firstDay-1) % 7 == 0){
-			//document.td.style.bebackgroundColor = "red" + d;
-			td.innerHTML = "<font color=#7ED5E4>" + d;
+			//td.bgColor = 'blue';
+			td.style.backgroundColor = 'blue';
+			td.innerHTML = "<font color=#FFEA00>" + d;
+			
 		}
 		//일요일은 배경색을 빨간색
 		if((d+firstDay-1) % 7 == 1){
-			td.innerHTML = "<font color=#FF9090>" + d;
+			td.style.backgroundColor = 'red';
+			td.innerHTML = "<font color=#FFEA00>" + d;
 		}
 	}
-	thd.appendChild(tr);
+	tbd.appendChild(tr);
 	
 	tbl.appendChild(thd);
 	tbl.appendChild(tbd);
@@ -89,4 +100,4 @@ function makeCalendar(month = 6){
 	document.getElementById('show').appendChild(tbl);
 	
 }
-makeCalendar(6) //호출해야 실행가능
+//makeCalendar(6) //호출해야 실행가능
